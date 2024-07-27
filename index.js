9e2108b05ed2b74a208fdcc136197cf6f9e3f999
Mya Weigandt <myaweigandt@Myas-MacBook-Air.local>
2024-07-27 12:48:40 -0400

    ignore node modules

--- index.js
+++ index.js
@@ -2,3 +2,54 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
+
+// prompts
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe what your project does"
+    },
+    {
+        type: "checkbox",
+        name: "lisense",
+        message: "Select a license applicable to this project",
+        choices: ["MIT","APACHE2.0","BSD2","BSD3","none"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are ther steps required to install your project"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use"
+    },
+
+    {
+        type: "input",
+        name: "credits",
+        message: "List your contributors"
+    },
+    {
+        type: "input",
+        name: "features",
+        message: "List any features you would like"
+    },
+    {
+        type: "input",
+        name: "how to contribute",
+        message: "How can a developer contribute to this project"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Provide walkthrough of test"
+    }, 
+]
